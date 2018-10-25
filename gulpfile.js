9125077d62cc85eb17af6a7b74a9e461f3dcdcc6
tsmsogn <tsmsogn@gmail.com>
2018-10-25 23:15:14 +0900

    環境構築

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var sass = require('gulp-sass');
+var pug = require('gulp-pug');
+var browserSync = require('browser-sync').create();
+var notify = require('gulp-notify');
+
+gulp.task('sass', function () {
+  gulp.src('./src/scss/*.scss')
+    .pipe(plumber({
+      errorHandler: notify.onError('<%= error.message %>')
+    }))
+    .pipe(sass())
+    .pipe(gulp.dest('./dest/assets/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+gulp.task('pug', function () {
+  gulp.src(
+    ['src/pug/**/*.pug', '!' + 'src/pug/**/_*.pug']
+  )
+    .pipe(plumber({
+      errorHandler: notify.onError('<%= error.message %>')
+    }))
+    .pipe(pug({pretty: true}))
+    .pipe(gulp.dest('./dest'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./src/scss/**/*.scss', ['sass']);
+  gulp.watch('./src/pug/**/*.pug', ['pug']);
+});
+
+gulp.task('browser-sync', function () {
+  browserSync.init({
+    server: {
+      baseDir: './dest'
+    }
+  });
+});
+
+gulp.task('default', ['browser-sync', 'watch']);
